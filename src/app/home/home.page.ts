be4bb9157b0122f6c273ea5d1ecf21336456c83d
omoz4real <omoz4real@yahoo.com>
2019-04-13 02:41:51 +0100

    added master detail routingu

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,8 @@ import { switchMap } from "rxjs/operators";
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
 
+import { HttpClient } from '@angular/common/http'
+
 
 @Component({
   selector: 'app-home',
@@ -14,31 +16,25 @@ import { DataService } from '../services/data.service';
 })
 export class HomePage implements OnInit {
     
-    constructor(
-    private dataservice: DataService,
-    private router: Router,
-    private route: ActivatedRoute
-    ) 
+    constructor( private dataservice: DataService,private router: Router,private http: HttpClient,private route: ActivatedRoute) 
     {}
     
     posts$: Observable<any>;
+    
+    films: Observable<any>;
+    
     loadPost(post: any) {
     this.router.navigate(["/post", post.id]);
   }
+  
 ngOnInit(){
-//   this.dataservice.getRemoteData().subscribe( data=>{
-//   console.log("Remote Data");
-//   console.log(data);
-//   
-//   }
-//   );
-    this.posts$ = this.route.paramMap.pipe(
-      switchMap(
-        (params: ParamMap) => this.dataservice.getRemoteDatas()
-      )
-    );
-}
-
+    this.films = this.http.get("http://localhost:8080/SimpleService/webresources/suggestedNameService");    
+  }
+    
+  openDetails(film) {
+      this.router.navigate(["/post", film.id]);
+//    this.router.navigateByUrl(`/post/{$id}`);
+  }
 
 
 }
--- src/app/post-detail/post-detail.page.ts
+++ src/app/post-detail/post-detail.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 import { ActivatedRoute, ParamMap } from "@angular/router";
 import { Observable } from "rxjs";
 import { switchMap } from "rxjs/operators";
@@ -12,15 +14,23 @@ import { DataService } from '../services/data.service';
 })
 export class PostDetailPage implements OnInit {
 
-  constructor(private route: ActivatedRoute, private dataSrvc: DataService) { }
+  constructor(private route: ActivatedRoute, private dataSrvc: DataService, private http: HttpClient) { }
 
 post$: Observable<any>;
 
+film: any;
+
   ngOnInit() {
       
-      this.post$ = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) => this.dataSrvc.fetchPost(params.get("id")))
-    );
+      let id = this.route.snapshot.paramMap.get('id');
+      this.http.get(`http://localhost:8080/SimpleService/webresources/suggestedNameService/${id}`).subscribe(res => {
+      this.film = res;
+    });
+      
+      
+//      this.post$ = this.route.paramMap.pipe(
+//      switchMap((params: ParamMap) => this.dataSrvc.fetchPost(params.get("id")))
+//    );
     
   }
 
